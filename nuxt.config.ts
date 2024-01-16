d423c14b8c33dfae1e9ac54034a605aedf0aca31
Ricky <rickyblopes@gmail.com>
2024-01-16 18:35:24 -0300

    fix: nuuxt connfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', "@nuxt/image"],
-  app: {
-    baseURL: '/nuxt-github-pages/', // baseURL: '/<repository>/'
-    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-  }
+  modules: ['@nuxt/ui', "@nuxt/image"]
 })
